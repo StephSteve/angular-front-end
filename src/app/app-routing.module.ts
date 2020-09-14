1e7261522e5dd464c0d3dd8306827f9f0bce95ce
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-09-14 11:06:39 -0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {Routes} from '@angular/router';
+import { AdminComponent} from './components/admin.admin.component';
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   declarations: [],
